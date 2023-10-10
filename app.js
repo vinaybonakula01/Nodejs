c97e711189f0450648b51db1a5012eb5be842d45
bonakulavinay <vinaybonakula5101@gmail.com>
2023-10-10 13:46:04 +0530

    using express j
    using express j

--- app.js
+++ app.js
@@ -1,34 +1,11 @@
-const http=require('http')
-const fs = require('fs');
-const server=http.createServer((req,res)=>{
-    const url=req.url;
-    const method =req.method;
-    if(url==='/'){
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type ="submit">send</button></input></form></body>');
-        res.write('</html>');
-        return res.end();
+const http=require('http');
+const express=require('express');
+const app=express();
+app.use('/product',(req,res,next)=>{
+    res.send('hello guys');
+})
+app.use('/',(req,res,next)=>{
+    res.send('hello');
+})
 
-    }
-   
-    if (url==='/message' && method==='post'){
-        const body=[];
-        req.on('data',(chunk)=>{
-            body.push(chunk);
-    
-        });
-        req.on('end',()=>{
-            const parsedbody=Buffer.concat(body).toString();
-            const message=parsedbody.split('=')[1];
-            fs.writeFileSync('message.txt',message);
-            res.statusCode=302;
-            res.setHeader('Location','/');
-            return res.end()
-        });
-      
-
-    }
-
-});
-server.listen(9000);
+app.listen(8000);
