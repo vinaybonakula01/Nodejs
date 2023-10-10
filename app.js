25e7722e9c8b541e06c26a59f5e4b0f16934b617
bonakulavinay <vinaybonakula5101@gmail.com>
2023-10-10 17:51:44 +0530

    Update

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const http=require('http');
 const express=require('express');
+
+const bodyParser = require('body-parser');
 const app=express();
-app.use('/product',(req,res,next)=>{
-    res.send('hello guys');
-})
-app.use('/',(req,res,next)=>{
-    res.send('hello');
+
+app.use(bodyParser.urlencoded({extended:false}));
+const adminrouter=require('./routes/admin');
+const shoprouter=require('./routes/shop');
+
+app.use(adminrouter);
+app.use(shoprouter);
+
+app.use((req,res,next)=>{
+   res.send('page not found')
+ 
 })
 
 app.listen(8000);
---
+++ routes/admin.js
@@ -0,0 +1,13 @@
+const express=require('express');
+const router=express.Router();
+const path=require('path');
+
+router.get('/add-product',(req,res)=>{
+    res.sendFile(path.join(__dirname,'../','views','add-product.html'))
+})
+router.post('/product',(req,res,next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+module.exports=router
